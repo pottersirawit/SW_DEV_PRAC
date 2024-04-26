c768623e8ebe96d713a81b4b18e88c8be487c5a2
Sirawit Pulketkij <sirawit.pulketkij@gmail.com>
2024-04-26 21:00:12 +0700

    fix: typo

--- models/Dentist.js
+++ models/Dentist.js
@@ -1,36 +1,43 @@
 const mongoose = require("mongoose");
-const DentistSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
-    unique: true,
-    trim: true,
-    maxlength: [50, "Name can not be more than 50 characters"],
+const DentistSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+      unique: true,
+      trim: true,
+      maxlength: [50, "Name can not be more than 50 characters"],
+    },
+    exp: {
+      type: Number,
+      required: [true, "Please add years of experience"],
+    },
+    area: {
+      type: String,
+      required: [true, "Please add an area of expertise"],
+    },
   },
-  exp: {
-    type: Number,
-    required: [true, "Please add years of experience"],
-  },
-  area: {
-    type: String,
-    required: [true, "Please add an area of expertise"],
-  },
-},{
-    toJSON : {virtuals: true},
-    toObject: {virtuals: true}
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+DentistSchema.pre(
+  "deleteOne",
+  { document: true, query: false },
+  async function (next) {
+    console.log(`Bookings being removed from dentist ${this._id}`);
+    await this, model("Booking").deleteMany({ dentist: this._id });
+    next();
+  }
+);
+
+DentistSchema.virtual("bookings", {
+  ref: "Booking",
+  localField: "_id",
+  foreignField: "dentist",
+  justOne: false,
 });
-
-DentistSchema.pre('deleteOne',{document: true, query: false}, async function(next){
-  console.log(`Bookings being removed from dentist ${this._id}`);
-  await this,model('Booking').deleteMany({dentist: this._id});
-  next();
-})
-
-DentistSchema.virtual('bookings',{
-  ref: 'Booking',
-  localField: '_id',
-  foreignField: 'hospital',
-  justOne: false
-})
 
 module.exports = mongoose.model("Dentist", DentistSchema);
